d281df6ed286997d81b47611052b6fdb7d816abf
Carlos HBP <carloshbp@hotmail.com>
2020-12-19 14:56:39 -0300

    Add Welcome Stories

---
+++ src/stories/0-Welcome.stories.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const teste = {
+    title: "Introduction/Welcome",
+};
+
+export default teste;
+
+export const toAutoEscolaNardini = () => (
+    <div>
+        <h1>Auto Escola Nardini</h1>
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo, unde,
+            excepturi libero sequi minus amet aspernatur maiores nam qui eos
+            dolore explicabo voluptates eaque exercitationem velit sed
+            voluptatum ea! Est.
+        </p>
+    </div>
+);
