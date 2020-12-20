61c1e7224abae8c07bb6db2d3191d51b9b9bce3b
Carlos HBP <carloshbp@hotmail.com>
2020-12-20 20:24:09 -0300

    Add Feature Component

---
+++ src/components/atoms/Feature.stories.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { FaCar, FaKey, FaMapMarkerAlt, FaAccessibleIcon } from "react-icons/fa";
+
+import Feature from "./Feature";
+import Grid from "components/atoms/Grid";
+
+const story = {
+    title: "Components/Atoms/Feature",
+    component: Feature,
+};
+
+export default story;
+
+export const usage = () => (
+    <Feature icon={<FaCar />} title="Título">
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+    </Feature>
+);
+
+export const withGrid = () => (
+    <Grid sm={2} md={4}>
+        <Feature icon={<FaCar />} title="Maior e melhor frota">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+        </Feature>
+        <Feature icon={<FaKey />} title="Habilitação mais rápida">
+            <p>Lorem ipsum.</p>
+        </Feature>
+        <Feature icon={<FaMapMarkerAlt />} title="Excelente localização">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+        </Feature>
+        <Feature icon={<FaAccessibleIcon />} title="Acessibilidade">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+        </Feature>
+    </Grid>
+);
