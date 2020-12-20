54a8c93b07366b29a6df4ec6af0300156b4031a6
Carlos HBP <carloshbp@hotmail.com>
2020-12-20 20:23:47 -0300

    Add Grid Component

---
+++ src/components/atoms/Grid.stories.js
@@ -0,0 +1,30 @@
+import React from "react";
+import styled from "styled-components";
+
+import Grid from "./Grid";
+
+const story = {
+    title: "Components/Atoms/Grid",
+    component: Grid,
+};
+
+export default story;
+
+const Box = styled.div`
+    height: 200px;
+    background-color: indigo;
+`;
+
+export const usage = () => (
+    <Grid sm={2} md={3} lg={4} xl={5}>
+        <Box></Box>
+        <Box></Box>
+        <Box></Box>
+        <Box></Box>
+
+        <Box></Box>
+        <Box></Box>
+        <Box></Box>
+        <Box></Box>
+    </Grid>
+);
